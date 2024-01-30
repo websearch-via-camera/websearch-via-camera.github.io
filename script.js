d8127aeefda6578460e2ef6dbbcb2cfa0a3a4a3f
websearch-via-camera <157770826+websearch-via-camera@users.noreply.github.com>
2024-01-30 12:42:32 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const { useState, useEffect } = React;
+const { useState, useEffect, useCallback } = React;
 
 const data = [{ id: 0, label: "English" }, { id: 1, label: "Spanish" }, { id: 2, label: "Indonesian" }, { id: 3, label: "Greek" }];
 
+const results = [
+{ href: 'https://websearch-via-camera.com/The%20Ten%20Commandments', title: 'The Ten Commandments' }];
+
+// const results = [
+//     { href: 'https://websearch-via-camera.com/The%20Ten%20Commandments', title: 'The Ten Commandments'}, { href: '#', title: 'The Psalms'},{ href: '#', title: 'The Quran'},
+// ]
 
 const Dropdown = () => {
   const [isOpen, setOpen] = useState(false);
   const [items, setItem] = useState(data);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [newName, setnewName] = useState({ href: '#', title: ' ' });
+
+  const shuffle = useCallback(() => {
+    const index = Math.floor(Math.random() * results.length);
+    setnewName(results[index]);
+  }, []);
+
+  useEffect(() => {
+    const intervalID = setInterval(shuffle, 2000);
+    return () => clearInterval(intervalID);
+  }, [shuffle]);
+
 
   const toggleDropdown = () => setOpen(!isOpen);
 
   const handleItemClick = id => {
     selectedItem == id ? setSelectedItem(null) : setSelectedItem(id);
+    toggleDropdown();
   };
 
   return /*#__PURE__*/(
     React.createElement("div", null, /*#__PURE__*/
-    React.createElement("h1", null, "Search"), /*#__PURE__*/
+    React.createElement("h1", null, "Search the Web"), /*#__PURE__*/
+    React.createElement("h3", null, "via your phone camera"), /*#__PURE__*/
+    React.createElement("h5", null, "Powered by GPT Vision"), /*#__PURE__*/
     React.createElement("div", { className: "dropdown" }, /*#__PURE__*/
     React.createElement("div", { className: "dropdown-header", onClick: toggleDropdown },
     selectedItem ? items.find(item => item.id == selectedItem).label : "Select your language", /*#__PURE__*/
@@ -26,9 +47,21 @@ const Dropdown = () => {
     items.map((item) => /*#__PURE__*/
     React.createElement("div", { className: "dropdown-item", onClick: e => handleItemClick(e.target.id), id: item.id }, /*#__PURE__*/
     React.createElement("span", { className: `dropdown-item-dot ${item.id == selectedItem && 'selected'}` }, "\u2022 "),
-    item.label))))));
+    item.label)))), /*#__PURE__*/
 
 
+
+
+
+    React.createElement("div", { style: { padding: '25px', paddingLeft: 0 } }, "Example results: ", /*#__PURE__*/
+
+    React.createElement("a", { href: newName.href }, newName.title)), /*#__PURE__*/
+
+    React.createElement("a", { style: { textDecoration: 'none' }, href: "page.html" }, /*#__PURE__*/
+    React.createElement("div", {
+      className: "start" }, /*#__PURE__*/
+
+    React.createElement("div", { style: { padding: '33px', underline: 'None' } }, " Start")))));
 
 
 
