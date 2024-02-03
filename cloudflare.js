220ae34bc4273e6e15e59f28936a83025e60a33f
websearch-via-camera <157770826+websearch-via-camera@users.noreply.github.com>
2024-02-02 23:01:51 -0600

    Update cloudflare.js

--- cloudflare.js
+++ cloudflare.js
@@ -126,6 +126,20 @@ export default {
      <meta name="viewport" content="width=device-width, initial-scale=0.8">
      <link href="https://websearch-via-camera.com/output.css" rel="stylesheet">
 
+
+     <script>
+      !function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="79adOenzquzD1LH2qjcQiGWBMqgWUAwU";;analytics.SNIPPET_VERSION="5.2.0";
+      analytics.load("79adOenzquzD1LH2qjcQiGWBMqgWUAwU");
+      analytics.page();
+      }}();
+    </script>
+
+     <link rel="apple-touch-icon" sizes="180x180" href="https://websearch-via-camera.com/apple-touch-icon.png">
+     <link rel="manifest" href="https://websearch-via-camera.com/site.webmanifest">
+     <link rel="icon" type="image/png" sizes="32x32" href="https://websearch-via-camera.com/favicon-32x32.png">
+     <link rel="icon" type="image/png" sizes="16x16" href="https://websearch-via-camera.com/favicon-16x16.png">
+     <link rel="icon" href="https://websearch-via-camera.com/favicon.ico">
+
      <script type="text/javascript">
      const jsn =  JSON.stringify( ${  JSON.stringify(json.webPages)} );
      async function getText(url, query) {
