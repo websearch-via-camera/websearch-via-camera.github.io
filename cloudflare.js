e5c3febe39ae1060bd33cc958c7ec8388d23e7c2
websearch-via-camera <157770826+websearch-via-camera@users.noreply.github.com>
2024-02-03 08:35:43 -0600

    Update cloudflare.js

--- cloudflare.js
+++ cloudflare.js
@@ -127,12 +127,9 @@ export default {
      <link href="https://websearch-via-camera.com/output.css" rel="stylesheet">
 
 
-     <script>
-      !function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="79adOenzquzD1LH2qjcQiGWBMqgWUAwU";;analytics.SNIPPET_VERSION="5.2.0";
-      analytics.load("79adOenzquzD1LH2qjcQiGWBMqgWUAwU");
-      analytics.page();
-      }}();
-    </script>
+     <script src="https://cdn.intake-lr.com/LogRocket.min.js" crossorigin="anonymous"></script>
+<script>window.LogRocket && window.LogRocket.init('rikjv0/websearch-via-camera');</script>
+
 
      <link rel="apple-touch-icon" sizes="180x180" href="https://websearch-via-camera.com/apple-touch-icon.png">
      <link rel="manifest" href="https://websearch-via-camera.com/site.webmanifest">
