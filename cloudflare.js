40a2e83bcb0cd259525798d6c087b7ce92afa9b3
Kiarash Adl <157770826+websearch-via-camera@users.noreply.github.com>
2024-02-18 02:14:14 -0600

    Update cloudflare.js

--- cloudflare.js
+++ cloudflare.js
@@ -217,8 +217,8 @@ export default {
      <link href="https://websearch-via-camera.com/output.css" rel="stylesheet">
 
 
-     <script src="https://cdn.intake-lr.com/LogRocket.min.js" crossorigin="anonymous"></script>
-<script>window.LogRocket && window.LogRocket.init('rikjv0/websearch-via-camera');</script>
+//      <script src="https://cdn.intake-lr.com/LogRocket.min.js" crossorigin="anonymous"></script>
+// <script>window.LogRocket && window.LogRocket.init('rikjv0/websearch-via-camera');</script>
 
 
 
@@ -284,198 +284,176 @@ export default {
     
      </script>
 
-  <style>
-  .resp-sharing-button__link,
-.resp-sharing-button__icon {
-display: inline-block
-}
-
-.resp-sharing-button__link {
-text-decoration: none;
-color: #fff;
-margin: 0.5em
-}
-
-.resp-sharing-button {
-border-radius: 5px;
-transition: 25ms ease-out;
-padding: 0.5em 0.75em;
-font-family: Helvetica Neue,Helvetica,Arial,sans-serif
-}
-
-.resp-sharing-button__icon svg {
-width: 1em;
-height: 1em;
-margin-right: 0.4em;
-vertical-align: top
-}
-
-.resp-sharing-button--small svg {
-margin: 0;
-vertical-align: middle
-}
-
-/* Non solid icons get a stroke */
-.resp-sharing-button__icon {
-stroke: #fff;
-fill: none
-}
-
-/* Solid icons get a fill */
-.resp-sharing-button__icon--solid,
-.resp-sharing-button__icon--solidcircle {
-fill: #fff;
-stroke: none
-}
-
-.resp-sharing-button--twitter {
-background-color: #55acee
-}
-
-.resp-sharing-button--twitter:hover {
-background-color: #2795e9
-}
-
-.resp-sharing-button--pinterest {
-background-color: #bd081c
-}
-
-.resp-sharing-button--pinterest:hover {
-background-color: #8c0615
-}
-
-.resp-sharing-button--facebook {
-background-color: #3b5998
-}
-
-.resp-sharing-button--facebook:hover {
-background-color: #2d4373
-}
-
-.resp-sharing-button--tumblr {
-background-color: #35465C
-}
-
-.resp-sharing-button--tumblr:hover {
-background-color: #222d3c
-}
-
-.resp-sharing-button--reddit {
-background-color: #5f99cf
-}
-
-.resp-sharing-button--reddit:hover {
-background-color: #3a80c1
-}
-
-.resp-sharing-button--google {
-background-color: #dd4b39
-}
-
-.resp-sharing-button--google:hover {
-background-color: #c23321
-}
-
-.resp-sharing-button--linkedin {
-background-color: #0077b5
-}
-
-.resp-sharing-button--linkedin:hover {
-background-color: #046293
-}
-
-.resp-sharing-button--email {
-background-color: #777
-}
-
-.resp-sharing-button--email:hover {
-background-color: #5e5e5e
-}
-
-.resp-sharing-button--xing {
-background-color: #1a7576
-}
-
-.resp-sharing-button--xing:hover {
-background-color: #114c4c
-}
-
-.resp-sharing-button--whatsapp {
-background-color: #25D366
-}
-
-.resp-sharing-button--whatsapp:hover {
-background-color: #1da851
-}
-
-.resp-sharing-button--hackernews {
-background-color: #FF6600
-}
-.resp-sharing-button--hackernews:hover, .resp-sharing-button--hackernews:focus {   background-color: #FB6200 }
-
-.resp-sharing-button--vk {
-background-color: #507299
-}
-
-.resp-sharing-button--vk:hover {
-background-color: #43648c
-}
-
-.resp-sharing-button--facebook {
-background-color: #3b5998;
-border-color: #3b5998;
-}
-
-.resp-sharing-button--facebook:hover,
-.resp-sharing-button--facebook:active {
-background-color: #2d4373;
-border-color: #2d4373;
-}
-
-.resp-sharing-button--twitter {
-background-color: #55acee;
-border-color: #55acee;
-}
-
-.resp-sharing-button--twitter:hover,
-.resp-sharing-button--twitter:active {
-background-color: #2795e9;
-border-color: #2795e9;
-}
-
-.resp-sharing-button--email {
-background-color: #777777;
-border-color: #777777;
-}
-
-.resp-sharing-button--email:hover,
-.resp-sharing-button--email:active {
-background-color: #5e5e5e;
-border-color: #5e5e5e;
-}
-
-.resp-sharing-button--linkedin {
-background-color: #0077b5;
-border-color: #0077b5;
-}
-
-.resp-sharing-button--linkedin:hover,
-.resp-sharing-button--linkedin:active {
-background-color: #046293;
-border-color: #046293;
-}
-
-.resp-sharing-button--whatsapp {
-background-color: #25D366;
-border-color: #25D366;
-}
-
-.resp-sharing-button--whatsapp:hover,
-.resp-sharing-button--whatsapp:active {
-background-color: #1DA851;
-border-color: #1DA851;
-}
-
+  <style>.resp-sharing-button__link,
+  .resp-sharing-button__icon {
+    display: inline-block
+  }
+  
+  .resp-sharing-button__link {
+    text-decoration: none;
+    color: #fff;
+    margin: 0.5em
+  }
+  
+  .resp-sharing-button {
+    border-radius: 5px;
+    transition: 25ms ease-out;
+    padding: 0.5em 0.75em;
+    font-family: Helvetica Neue,Helvetica,Arial,sans-serif
+  }
+  
+  .resp-sharing-button__icon svg {
+    width: 1em;
+    height: 1em;
+    margin-right: 0.4em;
+    vertical-align: top
+  }
+  
+  .resp-sharing-button--small svg {
+    margin: 0;
+    vertical-align: middle
+  }
+  
+  /* Non solid icons get a stroke */
+  .resp-sharing-button__icon {
+    stroke: #fff;
+    fill: none
+  }
+  
+  /* Solid icons get a fill */
+  .resp-sharing-button__icon--solid,
+  .resp-sharing-button__icon--solidcircle {
+    fill: #fff;
+    stroke: none
+  }
+  
+  .resp-sharing-button--twitter {
+    background-color: #55acee
+  }
+  
+  .resp-sharing-button--twitter:hover {
+    background-color: #2795e9
+  }
+  
+  .resp-sharing-button--pinterest {
+    background-color: #bd081c
+  }
+  
+  .resp-sharing-button--pinterest:hover {
+    background-color: #8c0615
+  }
+  
+  .resp-sharing-button--facebook {
+    background-color: #3b5998
+  }
+  
+  .resp-sharing-button--facebook:hover {
+    background-color: #2d4373
+  }
+  
+  .resp-sharing-button--tumblr {
+    background-color: #35465C
+  }
+  
+  .resp-sharing-button--tumblr:hover {
+    background-color: #222d3c
+  }
+  
+  .resp-sharing-button--reddit {
+    background-color: #5f99cf
+  }
+  
+  .resp-sharing-button--reddit:hover {
+    background-color: #3a80c1
+  }
+  
+  .resp-sharing-button--google {
+    background-color: #dd4b39
+  }
+  
+  .resp-sharing-button--google:hover {
+    background-color: #c23321
+  }
+  
+  .resp-sharing-button--linkedin {
+    background-color: #0077b5
+  }
+  
+  .resp-sharing-button--linkedin:hover {
+    background-color: #046293
+  }
+  
+  .resp-sharing-button--email {
+    background-color: #777
+  }
+  
+  .resp-sharing-button--email:hover {
+    background-color: #5e5e5e
+  }
+  
+  .resp-sharing-button--xing {
+    background-color: #1a7576
+  }
+  
+  .resp-sharing-button--xing:hover {
+    background-color: #114c4c
+  }
+  
+  .resp-sharing-button--whatsapp {
+    background-color: #25D366
+  }
+  
+  .resp-sharing-button--whatsapp:hover {
+    background-color: #1da851
+  }
+  
+  .resp-sharing-button--hackernews {
+  background-color: #FF6600
+  }
+  .resp-sharing-button--hackernews:hover, .resp-sharing-button--hackernews:focus {   background-color: #FB6200 }
+  
+  .resp-sharing-button--vk {
+    background-color: #507299
+  }
+  
+  .resp-sharing-button--vk:hover {
+    background-color: #43648c
+  }
+  
+  .resp-sharing-button--email {
+    background-color: #777777;
+    border-color: #777777;
+  }
+  
+  .resp-sharing-button--email:hover,
+  .resp-sharing-button--email:active {
+    background-color: #5e5e5e;
+    border-color: #5e5e5e;
+  }
+  
+  .resp-sharing-button--linkedin {
+    background-color: #0077b5;
+    border-color: #0077b5;
+  }
+  
+  .resp-sharing-button--linkedin:hover,
+  .resp-sharing-button--linkedin:active {
+    background-color: #046293;
+    border-color: #046293;
+  }
+  
+  .resp-sharing-button--whatsapp {
+    background-color: #25D366;
+    border-color: #25D366;
+  }
+  
+  .resp-sharing-button--whatsapp:hover,
+  .resp-sharing-button--whatsapp:active {
+    background-color: #1DA851;
+    border-color: #1DA851;
+  }
+  
+  
 </style>
 
           </head>
@@ -502,45 +480,25 @@ border-color: #1DA851;
     </h1>
     </a>
     <div class=" p-4">
-    <!-- Sharingbutton Facebook -->
-<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Facebook!">
-<div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="11.5"/><path d="M15.84 9.5H13.5V8.48c0-.53.35-.65.6-.65h1.4v-2.3h-2.35c-2.3 0-2.65 1.7-2.65 2.8V9.5h-2v2h2v7h3v-7h2.1l.24-2z"/></svg>
-  </div>
-</div>
-</a>
 
-<!-- Sharingbutton Twitter -->
-<a class="resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=GPT%20powered%20websearch%20results&amp;url=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on X!">
-<div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.5 7.4l-2 .2c-.4-.5-1-.8-2-.8C13.3 6.8 12 8 12 9.4v.6c-2 0-4-1-5.4-2.7-.2.4-.3.8-.3 1.3 0 1 .4 1.7 1.2 2.2-.5 0-1 0-1.2-.3 0 1.3 1 2.3 2 2.6-.3.4-.7.4-1 0 .2 1.4 1.2 2 2.3 2-1 1-2.5 1.4-4 1 1.3 1 2.7 1.4 4.2 1.4 4.8 0 7.5-4 7.5-7.5v-.4c.5-.4.8-1.5 1.2-2z"/><circle cx="12" cy="12" r="11.5"/></svg>
-  </div>
-</div>
-</a>
-
-<!-- Sharingbutton E-Mail -->
-<a class="resp-sharing-button__link" href="mailto:?subject=GPT%20powered%20websearch%20results&amp;body=https%3A%2F%2Fwebsearch-via-camera.com" target="_self" rel="noopener" aria-label="Send an Email!">
-<div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.5 16c0 .8-.7 1.5-1.5 1.5H6c-.8 0-1.5-.7-1.5-1.5V8c0-.8.7-1.5 1.5-1.5h12c.8 0 1.5.7 1.5 1.5v8zm-2-7.5L12 13 6.5 8.5m11 6l-4-2.5m-7 2.5l4-2.5"/><circle cx="12" cy="12" r="11.5"/></svg>
-  </div>
-</div>
-</a>
-
-<!-- Sharingbutton LinkedIn -->
-<a class="resp-sharing-button__link" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwebsearch-via-camera.com&amp;title=GPT%20powered%20websearch%20results&amp;summary=GPT%20powered%20websearch%20results&amp;source=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Linkedin!">
-<div class="resp-sharing-button resp-sharing-button--linkedin resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="11.5"/><path d="M15 12.5c-.28 0-.5.22-.5.5v3.5h-3s.03-6.48 0-7h3v.83s.46-.75 1.7-.75c1.56 0 2.3 1.12 2.3 3.3v3.62h-3V13c0-.28-.23-.5-.5-.5zm-7.5-3h2v7h-2z"/><circle cx="8.5" cy="6.5" r="1"/></svg>
-  </div>
-</div>
-</a>
-
-<!-- Sharingbutton WhatsApp -->
-<a class="resp-sharing-button__link" href="whatsapp://send?text=GPT%20powered%20websearch%20results%20https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Whatsapp!">
-<div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle xmlns="http://www.w3.org/2000/svg" cx="12" cy="12" r="11.5"/><path stroke-width="1px" d="M17.6 6.2c-1.5-1.5-3.4-2.3-5.5-2.3-4.3 0-7.8 3.5-7.8 7.8 0 1.4.4 2.7 1 3.9l-1.1 4 4.1-1.1c1.1.6 2.4.9 3.7.9 4.3 0 7.8-3.5 7.8-7.8.1-2-.7-3.9-2.2-5.4zm-5.5 11.9c-1.2 0-2.3-.3-3.3-.9l-.2-.1-2.4.6.7-2.4-.2-.2c-.6-1-1-2.2-1-3.4 0-3.6 2.9-6.5 6.5-6.5 1.7 0 3.3.7 4.6 1.9 1.2 1.2 1.9 2.8 1.9 4.6-.1 3.5-3 6.4-6.6 6.4zm3.5-4.8c-.2-.1-1.1-.6-1.3-.6-.2-.1-.3-.1-.4.1-.1.2-.5.6-.6.8-.1.1-.2.1-.4 0s-.8-.3-1.6-1c-.6-.5-1-1.2-1.1-1.3-.1-.2 0-.3.1-.4l.3-.3s.1-.2.2-.3c.1-.1 0-.2 0-.3s-.4-1.1-.6-1.4c-.2-.4-.3-.3-.4-.3h-.4s-.3 0-.5.2-.7.7-.7 1.6c0 1 .7 1.9.8 2s1.4 2.1 3.3 2.9c.5.2.8.3 1.1.4.5.1.9.1 1.2.1.4-.1 1.1-.5 1.3-.9.2-.5.2-.8.1-.9 0-.2-.2-.3-.4-.4z"/></svg>
-  </div>
-</div>
-</a>
+    <!-- Sharingbutton E-Mail -->
+    <a class="resp-sharing-button__link" href="mailto:?subject=Websearch%20via%20camera%20is%20a%20new%20way%20to%20Search!&amp;body=https%3A%2F%2Fwebsearch-via-camera.com" target="_self" rel="noopener" aria-label="E-Mail">
+      <div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--medium"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--normal">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.5 18c0 .8-.7 1.5-1.5 1.5H2c-.8 0-1.5-.7-1.5-1.5V6c0-.8.7-1.5 1.5-1.5h20c.8 0 1.5.7 1.5 1.5v12zm-3-9.5L12 14 3.5 8.5m0 7.5L7 14m13.5 2L17 14"/></svg></div>E-Mail</div>
+    </a>
+    
+    <!-- Sharingbutton LinkedIn -->
+    <a class="resp-sharing-button__link" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwebsearch-via-camera.com&amp;title=Websearch%20via%20camera%20is%20a%20new%20way%20to%20Search!&amp;summary=Websearch%20via%20camera%20is%20a%20new%20way%20to%20Search!&amp;source=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="LinkedIn">
+      <div class="resp-sharing-button resp-sharing-button--linkedin resp-sharing-button--medium"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--normal">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6.5 21.5h-5v-13h5v13zM4 6.5h-.04c-1.5 0-2.5-1.18-2.5-2.48 0-1.33 1.02-2.4 2.56-2.4s2.5 1.1 2.52 2.43c0 1.3-.98 2.45-2.55 2.45zm11.5 6c-1.1 0-2 .9-2 2v7h-5s.06-12 0-13h5V10s1.55-1.46 3.94-1.46c2.96 0 5.06 2.15 5.06 6.3v6.66h-5v-7c0-1.1-.9-2-2-2z"/></svg></div>LinkedIn</div>
+    </a>
+    
+    <!-- Sharingbutton WhatsApp -->
+    <a class="resp-sharing-button__link" href="whatsapp://send?text=Websearch%20via%20camera%20is%20a%20new%20way%20to%20Search!%20https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="WhatsApp">
+      <div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--medium"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--normal">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path stroke-width="1px" d="M20.1 3.9C17.9 1.7 15 .5 12 .5 5.8.5.7 5.6.7 11.9c0 2 .5 3.9 1.5 5.6L.6 23.4l6-1.6c1.6.9 3.5 1.3 5.4 1.3 6.3 0 11.4-5.1 11.4-11.4-.1-2.8-1.2-5.7-3.3-7.8zM12 21.4c-1.7 0-3.3-.5-4.8-1.3l-.4-.2-3.5 1 1-3.4L4 17c-1-1.5-1.4-3.2-1.4-5.1 0-5.2 4.2-9.4 9.4-9.4 2.5 0 4.9 1 6.7 2.8 1.8 1.8 2.8 4.2 2.8 6.7-.1 5.2-4.3 9.4-9.5 9.4zm5.1-7.1c-.3-.1-1.7-.9-1.9-1-.3-.1-.5-.1-.7.1-.2.3-.8 1-.9 1.1-.2.2-.3.2-.6.1s-1.2-.5-2.3-1.4c-.9-.8-1.4-1.7-1.6-2-.2-.3 0-.5.1-.6s.3-.3.4-.5c.2-.1.3-.3.4-.5.1-.2 0-.4 0-.5C10 9 9.3 7.6 9 7c-.1-.4-.4-.3-.5-.3h-.6s-.4.1-.7.3c-.3.3-1 1-1 2.4s1 2.8 1.1 3c.1.2 2 3.1 4.9 4.3.7.3 1.2.5 1.6.6.7.2 1.3.2 1.8.1.6-.1 1.7-.7 1.9-1.3.2-.7.2-1.2.2-1.3-.1-.3-.3-.4-.6-.5z"/></svg></div>WhatsApp</div>
+    </a>
+    
 
     </div>
     
