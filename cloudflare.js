d3d5fa76f3a142f26c87fcbf218f970789a88562
Kiarash Adl <157770826+websearch-via-camera@users.noreply.github.com>
2024-02-18 01:49:28 -0600

    Update cloudflare.js

--- cloudflare.js
+++ cloudflare.js
@@ -31,6 +31,7 @@ export default {
       console.log(path)
       console.log(lang)
       console.log(query)
+      let tryText = "Try Websearch via camera"
       let resultFor = "Results for"
       let slow = "Slow, please wait."
       let slowText = "GPT is doing work. This takes upto two minutes. The result will appear below!"
@@ -39,6 +40,7 @@ export default {
       let powered = "Powered by GPT."
       let apiurlsummary = "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-2b706faa-8009-4af8-9ba2-0d52f5a1bed1/default/doSummary";
       if(lang == 'es') {
+          tryText = "Pruebe la búsqueda web a través de la cámara"
           slow = "Lento, por favor espera."
           slowText = "GPT está trabajando. Esto lleva hasta dos minutos. ¡El resultado aparecerá a continuación!"
           GPTKnowledge = "Conocimiento GPT"
@@ -48,6 +50,7 @@ export default {
           apiurlsummary = 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-2b706faa-8009-4af8-9ba2-0d52f5a1bed1/default/doEsSummary'
       }
       if(lang == 'el') {
+           tryText = "Δοκιμάστε την Αναζήτηση στο Web μέσω κάμερας"
           slow = "Σιγά, παρακαλώ περιμένετε."
           slowText = "Το GPT κάνει δουλειά. Αυτό διαρκεί έως και δύο λεπτά. Το αποτέλεσμα θα εμφανιστεί παρακάτω!"
           GPTKnowledge = "Γνώση GPT"
@@ -57,6 +60,7 @@ export default {
           apiurlsummary = 'https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-2b706faa-8009-4af8-9ba2-0d52f5a1bed1/default/doElSummary'
       }
       if(lang == 'id') {
+          tryText = "Coba Penelusuran Web melalui kamera"
           slow = "Lambat, harap tunggu."
           slowText = "GPT sedang melakukan pekerjaan. Ini memakan waktu hingga dua menit. Hasilnya akan tampak di bawah!"
           GPTKnowledge = "pengetahuan GPT"
@@ -103,7 +107,7 @@ export default {
             let width = json.images?.value[i]?.thumbnail.width;
             let height = json.images?.value[i]?.thumbnail.height;
             console.log([contentUrl,followUrl,name ,thumbnailUrl,width, height])
-              let template = `<a href="${followUrl}"><img style="padding: 10px;" width=${width} height=${height} src="${thumbnailUrl}"/>
+              let template = `<a href="${followUrl}"><img  alt="${query}" style="padding: 10px;" width=${width} height=${height} src="${thumbnailUrl}"/>
               </a>`;
               photosPart += template;
           }
@@ -143,7 +147,9 @@ export default {
     <meta charset="UTF-8">
     <meta content="https://websearch-via-camera.com/logo.png" itemprop="image">
 
-     <meta name="viewport" content="width=device-width, initial-scale=0.8">
+     <meta name="viewport" content="width=device-width">
+     <title>${query} - Websearch via camera</title>
+
      <meta property="og:title" content="${query}">
      <meta property="og:site_name" content="Websearch via camera">
      <meta property="og:url" content="https://result.websearch-via-camera.com/${lang}/${query}">
@@ -176,9 +182,9 @@ export default {
      "@type":"WebSite",
      "image":{
         "@type":"ImageObject",
-        "height":"",
+        "height":"316",
         "url":"https://websearch-via-camera.com/logo.png",
-        "width":""
+        "width":"474"
      },
      "inLanguage":"${lang}",
      "name":"Websearch via camera",
@@ -198,7 +204,7 @@ export default {
            "@type":"ListItem",
            "item":"https://result.websearch-via-camera.com/${lang}/${query}",
            "name":"${query}",
-           "position":3
+           "position":2
         }
      ],
      "name":"Breadcrumbs"
@@ -475,26 +481,29 @@ border-color: #1DA851;
           </head>
   <body>
   <header style="display: flex; justify-content: center;">
-        <a href="https://websearch-via-camera.com"><img src="https://websearch-via-camera.com/logo.png" style="max-width:100%;" width="600" height="400"></a>
+        <a href="https://websearch-via-camera.com">
+        <img src="https://websearch-via-camera.com/logo.png" style="max-width:100%;" width="600" height="400" alt="Websearch via camera logo"></a>
   </header>
   <div style="
     justify-content: center;
     display: flex;
     justify-content: center;
 ">
-<a href="https://websearch-via-camera.com" target="_blank">
+<a href="https://websearch-via-camera.com/${lang}" target="_blank">
               <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                Try Websearch via camera
+                ${tryText}
               </button>
            </a></div>
   <!-- partial:index.partial.html -->
   <div class="bg-white grid place-items-center min-h-screen w-full">
+  <a href="https://result.websearch-via-camera.com/${lang}/${query}">
     <h1 style="color: crimson;" class="text-xl m3 p-4 font-bold underline">
       ${resultFor} "${query}".
     </h1>
+    </a>
     <div class=" p-4">
     <!-- Sharingbutton Facebook -->
-<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Facebook!">
 <div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="11.5"/><path d="M15.84 9.5H13.5V8.48c0-.53.35-.65.6-.65h1.4v-2.3h-2.35c-2.3 0-2.65 1.7-2.65 2.8V9.5h-2v2h2v7h3v-7h2.1l.24-2z"/></svg>
   </div>
@@ -502,7 +511,7 @@ border-color: #1DA851;
 </a>
 
 <!-- Sharingbutton Twitter -->
-<a class="resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=GPT%20powered%20websearch%20results&amp;url=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=GPT%20powered%20websearch%20results&amp;url=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on X!">
 <div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.5 7.4l-2 .2c-.4-.5-1-.8-2-.8C13.3 6.8 12 8 12 9.4v.6c-2 0-4-1-5.4-2.7-.2.4-.3.8-.3 1.3 0 1 .4 1.7 1.2 2.2-.5 0-1 0-1.2-.3 0 1.3 1 2.3 2 2.6-.3.4-.7.4-1 0 .2 1.4 1.2 2 2.3 2-1 1-2.5 1.4-4 1 1.3 1 2.7 1.4 4.2 1.4 4.8 0 7.5-4 7.5-7.5v-.4c.5-.4.8-1.5 1.2-2z"/><circle cx="12" cy="12" r="11.5"/></svg>
   </div>
@@ -510,7 +519,7 @@ border-color: #1DA851;
 </a>
 
 <!-- Sharingbutton E-Mail -->
-<a class="resp-sharing-button__link" href="mailto:?subject=GPT%20powered%20websearch%20results&amp;body=https%3A%2F%2Fwebsearch-via-camera.com" target="_self" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link" href="mailto:?subject=GPT%20powered%20websearch%20results&amp;body=https%3A%2F%2Fwebsearch-via-camera.com" target="_self" rel="noopener" aria-label="Send an Email!">
 <div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.5 16c0 .8-.7 1.5-1.5 1.5H6c-.8 0-1.5-.7-1.5-1.5V8c0-.8.7-1.5 1.5-1.5h12c.8 0 1.5.7 1.5 1.5v8zm-2-7.5L12 13 6.5 8.5m11 6l-4-2.5m-7 2.5l4-2.5"/><circle cx="12" cy="12" r="11.5"/></svg>
   </div>
@@ -518,7 +527,7 @@ border-color: #1DA851;
 </a>
 
 <!-- Sharingbutton LinkedIn -->
-<a class="resp-sharing-button__link" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwebsearch-via-camera.com&amp;title=GPT%20powered%20websearch%20results&amp;summary=GPT%20powered%20websearch%20results&amp;source=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=https%3A%2F%2Fwebsearch-via-camera.com&amp;title=GPT%20powered%20websearch%20results&amp;summary=GPT%20powered%20websearch%20results&amp;source=https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Linkedin!">
 <div class="resp-sharing-button resp-sharing-button--linkedin resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="11.5"/><path d="M15 12.5c-.28 0-.5.22-.5.5v3.5h-3s.03-6.48 0-7h3v.83s.46-.75 1.7-.75c1.56 0 2.3 1.12 2.3 3.3v3.62h-3V13c0-.28-.23-.5-.5-.5zm-7.5-3h2v7h-2z"/><circle cx="8.5" cy="6.5" r="1"/></svg>
   </div>
@@ -526,7 +535,7 @@ border-color: #1DA851;
 </a>
 
 <!-- Sharingbutton WhatsApp -->
-<a class="resp-sharing-button__link" href="whatsapp://send?text=GPT%20powered%20websearch%20results%20https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="">
+<a class="resp-sharing-button__link" href="whatsapp://send?text=GPT%20powered%20websearch%20results%20https%3A%2F%2Fwebsearch-via-camera.com" target="_blank" rel="noopener" aria-label="Share on Whatsapp!">
 <div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--circle">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle xmlns="http://www.w3.org/2000/svg" cx="12" cy="12" r="11.5"/><path stroke-width="1px" d="M17.6 6.2c-1.5-1.5-3.4-2.3-5.5-2.3-4.3 0-7.8 3.5-7.8 7.8 0 1.4.4 2.7 1 3.9l-1.1 4 4.1-1.1c1.1.6 2.4.9 3.7.9 4.3 0 7.8-3.5 7.8-7.8.1-2-.7-3.9-2.2-5.4zm-5.5 11.9c-1.2 0-2.3-.3-3.3-.9l-.2-.1-2.4.6.7-2.4-.2-.2c-.6-1-1-2.2-1-3.4 0-3.6 2.9-6.5 6.5-6.5 1.7 0 3.3.7 4.6 1.9 1.2 1.2 1.9 2.8 1.9 4.6-.1 3.5-3 6.4-6.6 6.4zm3.5-4.8c-.2-.1-1.1-.6-1.3-.6-.2-.1-.3-.1-.4.1-.1.2-.5.6-.6.8-.1.1-.2.1-.4 0s-.8-.3-1.6-1c-.6-.5-1-1.2-1.1-1.3-.1-.2 0-.3.1-.4l.3-.3s.1-.2.2-.3c.1-.1 0-.2 0-.3s-.4-1.1-.6-1.4c-.2-.4-.3-.3-.4-.3h-.4s-.3 0-.5.2-.7.7-.7 1.6c0 1 .7 1.9.8 2s1.4 2.1 3.3 2.9c.5.2.8.3 1.1.4.5.1.9.1 1.2.1.4-.1 1.1-.5 1.3-.9.2-.5.2-.8.1-.9 0-.2-.2-.3-.4-.4z"/></svg>
   </div>
@@ -538,7 +547,7 @@ border-color: #1DA851;
 
     <div id="buttons" class="inline-flex divide-x divide-dashed border p-10 rounded">
       <div>
-        <button id="knowledgeBtn" onclick="showGPT(' ${query.replace(/'/g, "")} ')" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-10">
+        <button id="knowledgeBtn" onclick="showGPT(' ${query.replace(/'/g, "")} ')" style="background-color: #1d4ed8;color: white;" class="hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-10">
         ${GPTKnowledge}
         </button>
       </div>
