6ddc9dffb6d83f8348f4859b05cf72879f39a694
Kiarash Adl <157770826+websearch-via-camera@users.noreply.github.com>
2024-02-19 00:40:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ const Dropdown = () => {
     React.createElement("h4", null, "Powered by GPT Vision"), /*#__PURE__*/
     React.createElement("a", { href: "https://websearch-via-camera.com/privacy%20policy.html" }, "Privacy Policy"),
                        
-    React.createElement("a", { style: { paddingLeft: 100} }, {href: "https://websearch-via-camera.com/result.html" }, "Sitemap")));
+    React.createElement("a", {href: "https://websearch-via-camera.com/result.html" }, "Sitemap")));
 
 
 };
